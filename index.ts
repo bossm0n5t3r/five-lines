2f7981cfa3684bdbdf9cc7dfcf1d21b241003dac
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-01-22 22:49:20 +0900

    Extract method with good name

--- index.ts
+++ index.ts
@@ -124,12 +124,16 @@ function update() {
   }
 }
 
-function draw() {
+const createGraphics = () => {
   let canvas = document.getElementById('GameCanvas') as HTMLCanvasElement;
   let g = canvas.getContext('2d');
 
   g.clearRect(0, 0, canvas.width, canvas.height);
+  return g;
+};
 
+function draw() {
+  let g = createGraphics();
   drawMap(g);
   drawPlayer(g);
 }
