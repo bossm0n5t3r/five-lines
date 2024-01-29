427c901e4d1f520230944d3c4c7c4719f3bae673
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-01-30 02:24:40 +0900

    Strategy Pattern

--- index.ts
+++ index.ts
@@ -1153,14 +1153,21 @@ class RemoveLock1 implements RemoveStrategy {
   };
 }
 
+class RemoveLock2 implements RemoveStrategy {
+  check = (tile: Tile) => {
+    return tile.isLock2();
+  };
+}
+
 interface RemoveStrategy {
   check(tile: Tile): boolean;
 }
 
 const removeLock2 = () => {
+  let shouldRemove = new RemoveLock2();
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x].isLock2()) {
+      if (shouldRemove.check(map[y][x])) {
         map[y][x] = new Air();
       }
     }
