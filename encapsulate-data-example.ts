f4d96ea2aae9a3b1b81ffdde81940182d6d24eda
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-24 23:30:00 +0900

    ENCAPSULATE DATA

--- encapsulate-data-example.ts
+++ encapsulate-data-example.ts
@@ -1,12 +1,22 @@
-let counter = 0;
+class Counter {
+  private _counter = 0;
+  getCounter() {
+    return this._counter;
+  }
+  setCounter(c: number) {
+    this._counter = c;
+  }
+}
 
-const incrementCounter = () => {
-  counter++;
+let counter = new Counter();
+
+const incrementCounter = (counter: Counter) => {
+  counter.setCounter(counter.getCounter() + 1);
 };
 
 const main = () => {
   for (let i = 0; i < 10; i++) {
-    incrementCounter();
-    console.log(counter);
+    incrementCounter(counter);
+    console.log(counter.getCounter());
   }
 };
